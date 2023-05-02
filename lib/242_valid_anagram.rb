43b90f95c0db519f33398949858715f33330ea7a
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-02 08:52:36 -0600

    feat: working solution 242_valid_anagram

--- lib/242_valid_anagram.rb
+++ lib/242_valid_anagram.rb
@@ -1,8 +1,34 @@
 require 'pry'
 
 def is_anagram(s, t)
-    require 'pry'; binding.pry
+    # s_hash = Hash.new(0)
+    # t_hash = Hash.new(0)
+    # s.split('').map { |let| let +=1 }
+    # t.split('').map { |lett| lett += 1 }
+    # if s.split('').sort == t.split('').sort
+    #     true
+    # else
+    #     false
+    # end
+    hashmap = Hash.new(0)
+    if s.length != t.length
+        false
+    elsif
+        for lett in s.chars
+            hashmap[lett] += 1
+        end
+
+        for let in t.chars
+            hashmap[let] -= 1
+        end
+        if !hashmap.any? { |k,v| v != 0 }
+            true
+        else
+            false
+        end
+    end
 end
 
-is_anagram(s = "anagram", t = "nagaram")
-is_anagram(s = "rat", t = "car")
+test_1 = is_anagram(s = "anagram", t = "nagaram")
+test_2 = is_anagram(s = "rat", t = "car")
+require 'pry'; binding.pry

13279a41f0cdb7c13cc84bd4e18a95f7efdd7c4c
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-08 10:04:30 -0600

    refactor: remove pry

--- lib/347_top_k_frequent_elements.rb
+++ lib/347_top_k_frequent_elements.rb
@@ -7,9 +7,9 @@ def top_k_frequent(nums, k)
   nums.each { |num| hash[num] += 1}
   sorted = hash.sort_by { |k, v| -v }
   sorted.map { |a| a.first }.first(k)
+
 end
 
 test1 = top_k_frequent([1,1,1,2,2,3], 2)
 test2 = top_k_frequent([1], 1)
 test3 = top_k_frequent([1,1,1,2,2,3,4,4,4,4,5,5,5,5,5,5], 3)
-require 'pry'; binding.pry

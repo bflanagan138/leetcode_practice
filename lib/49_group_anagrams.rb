609e36acede833a80da9e106b4cfcbe530afdf94
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-08 09:51:13 -0600

    feat: complete challenge, passing with decent effeciency

--- lib/49_group_anagrams.rb
+++ lib/49_group_anagrams.rb
@@ -4,11 +4,9 @@ require 'pry'
 # if length of array is 1, return array nested inside array
 # group_by?
 def group_anagrams(strs)
-  # return [strs] if strs.length == 1
   strs.group_by { |s| s.chars.sort }.values
 end
 
 test1 = group_anagrams(["eat","tea","tan","ate","nat","bat"])
 test2 = group_anagrams([""])
 test3 = group_anagrams(["a"])
-require 'pry'; binding.pry

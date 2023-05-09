6cc50cf92ad37fdfe33f23b4667ac3d9ccd718f9
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-09 09:59:40 -0600

    feat: code passes if nums does not include zero

---
+++ lib/238_product_of_array_except_self.rb
@@ -0,0 +1,32 @@
+
+require 'pry'
+# iterate over array
+# for each multiply all elements
+# divide by current number
+
+def product_except_self(nums)
+  output = []
+  nums.each do |num| 
+    if !nums.include?(0)
+      output << ( nums.inject(:*) / num )
+      # nums.delete(num)
+      # output << nums.inject(:*)
+    else
+      container = []
+      numbers = nums
+      nums.each do |num|
+        numbers.delete_if do |num|
+          num == 0
+        end
+        # require 'pry'; binding.pry
+        output << numbers.inject(:*) 
+      end
+    end
+  end
+  output
+end
+
+test1 = product_except_self([1,2,3,4])
+test2 = product_except_self([-1,1,0,-3,3])
+# test3 = product_except_self([])
+require 'pry'; binding.pry

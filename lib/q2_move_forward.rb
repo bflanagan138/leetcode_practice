a256f0b88d989c5a73f90823edd121613aefc43e
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-20 22:39:35 -0600

    feat: add q2, 128 challenge starts

---
+++ lib/q2_move_forward.rb
@@ -0,0 +1,11 @@
+require 'pry'
+# output is our goal to reach
+# once output is reached, exit
+# looking for maximum, maximum is just equal to output as each move can be 1
+
+def moves(nums, output)
+  
+end
+
+test3 = moves_min([1,2,5,2,1,1])
+test4 = moves_min([4,2,1,5,1,2])

ad569dd305d4c9efb72689f0975f3bdf3fdfa443
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-07 08:49:49 -0600

    setup: Sets up challenge

---
+++ lib/167_two_sum_II.rb
@@ -0,0 +1,5 @@
+require 'pry'
+
+def two_sum(numbers, target)
+    
+end
--- lib/20_valid_parenthesis.rb
+++ lib/20_valid_parenthesis.rb
@@ -1,17 +1,20 @@
 require 'pry'
-
+#create a hash including opening parenthesis as keys, closing parenthesis as values
+#create 
+parens = { "(" => ")", "{" => "}", "[" => "]" }
+opening_parens = parens.keys
+closing_parens = parens.values
+require 'pry'; binding.pry
 def is_valid(s)
-  chars = s.split('')
-  chars.each_with_index do |c, i|
-    if c == '(' && c.next == ')'
-      true
-    elsif c == '[' && c.next == ']'
-      true
-    elsif c == '{' && c.next == '}'
-      true
-    else
-      false
-    end
+  stack = []
+  s.each_char do |char|
+    # if opening_parens.include?(char)
+    #   stack << char
+    # elsif closing_parens.include?(char)
+    #   if char == parens[stack.last]
+
+    #   end
+    # end
   end
 end
 

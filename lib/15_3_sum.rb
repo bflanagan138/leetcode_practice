e96d3943c229553bdb90fcebd17c226056925ebe
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-13 08:43:54 -0600

    feat: setup code, begin pseudocoding)

---
+++ lib/15_3_sum.rb
@@ -0,0 +1,11 @@
+require 'pry'
+# don't care about index
+# need to add 3 integers from array to make target of 0
+def three_sum(nums)
+    target = 0
+    nums
+end
+
+test1 = three_sum([-1,0,1,2,-1,-4])
+test2 = three_sum([0,1,1])
+test3 = three_sum([0,0,0])

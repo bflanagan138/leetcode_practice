ee9b61eb081018f6905fbab88dfe32f71b8fe3de
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-14 09:53:22 -0600

    feat: Adds working solution, times out

--- lib/15_3_sum.rb
+++ lib/15_3_sum.rb
@@ -1,11 +1,19 @@
 require 'pry'
-# don't care about index
-# need to add 3 integers from array to make target of 0
+
+# add all number groups to container, if array.add == 0 return array
 def three_sum(nums)
-    target = 0
-    nums
+  potential_answers = nums.combination(3).to_a
+  sorted_answers = []
+
+  potential_answers.each do |answer|
+    sorted_answers << answer.sort
+  end
+
+  correct_answers = sorted_answers.uniq.find_all { |answer| answer.sum == 0}
+  correct_answers
 end
 
 test1 = three_sum([-1,0,1,2,-1,-4])
 test2 = three_sum([0,1,1])
 test3 = three_sum([0,0,0])
+require 'pry'; binding.pry

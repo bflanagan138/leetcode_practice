b5bcb9ed8f899d1a4380f78b5c724688be3635ab
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-08-17 10:50:27 -0600

    feat: Solves challenge.)

--- lib/15_3_sum.rb
+++ lib/15_3_sum.rb
@@ -2,15 +2,22 @@ require 'pry'
 
 # add all number groups to container, if array.add == 0 return array
 def three_sum(nums)
-  potential_answers = nums.combination(3).to_a
+  potential_answers = nums.sort.combination(3).to_a.uniq
   sorted_answers = []
 
-  potential_answers.each do |answer|
-    sorted_answers << answer.sort
+  potential_answers.each_with_index do |ans, ind|
+    if ans.sum == 0 && sorted_answers.exclude?(ans)
+      sorted_answers << potential_answers[ind]
+    end
   end
+  require 'pry'; binding.pry
+  sorted_answers
+  # potential_answers.map do |answer|
+  #   sorted_answers << answer.sort
+  # end
 
-  correct_answers = sorted_answers.uniq.find_all { |answer| answer.sum == 0}
-  correct_answers
+  # correct_answers = sorted_answers.find_all { |answer| answer.sum == 0}
+  # correct_answers
 end
 
 test1 = three_sum([-1,0,1,2,-1,-4])
